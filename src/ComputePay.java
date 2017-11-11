66b9a4e2b53aee069c3a6c2cf9cb1c7bf2f53108
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2017-11-11 01:03:04 -0600

    Initialized values and declared variables

--- src/ComputePay.java
+++ src/ComputePay.java
@@ -6,13 +6,17 @@
 
 public class ComputePay {
 	public static void main(String[] args) {
+		int hours;
+		hours = (4 + 5 + 8 + 4);
 		System.out.println("My total hours worked:");
 		System.out.println(4 + 5 + 8 + 4);
 		
+		double salary;
+		salary = (8.75);
 		System.out.println("My hourly salary:");
 		System.out.println("$8.75");
 		
 		System.out.println("My total pay:");
-		System.out.println((4 + 5 + 8 + 4) * 8.75);
+		System.out.println(hours * salary);
 	}
 }
